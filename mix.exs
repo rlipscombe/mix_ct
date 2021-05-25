62226a3a93cac21fa0579ff55b9957305b80a1c6
Roger Lipscombe <roger@differentpla.net>
2021-05-25 09:18:15 +0100

    Fix 'current application does not depend' warning; bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MixCt.MixProject do
   def project do
     [
       app: :mix_ct,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -14,7 +14,7 @@ defmodule MixCt.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :eex]
     ]
   end
 
